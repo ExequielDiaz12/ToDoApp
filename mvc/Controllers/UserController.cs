0eff09f8012e5a7d6db68e5501faa9c111e82f59
Exequiel Diaz <exequieldiazbj@gmail.com>
2023-11-13 03:04:58 -0300

    edit users
    done edit users sections

--- mvc/Controllers/UserController.cs
+++ mvc/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using mvc.Models;
 using System.Net.Http;
 using System.Text.Json.Serialization;
 using System.Text.Json;
+using Newtonsoft.Json;
+using System.Text;
 
 
 namespace mvc.Controllers;
@@ -14,11 +16,11 @@ public class UserController : Controller
     private readonly ILogger<UserController> _logger;
     private readonly HttpClient _httpClient;
     public UserController(ILogger<UserController> logger,
-    HttpClient httpClient)
+    IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
-        _httpClient = httpClient;
-        httpClient.BaseAddress = new Uri("http://localhost:5184");
+        _httpClient = httpClientFactory.CreateClient();
+        _httpClient.BaseAddress = new Uri("http://localhost:5184");
     }
 
     public async Task<IActionResult> Index()
@@ -26,11 +28,67 @@ public class UserController : Controller
         var response = await _httpClient.GetAsync("/User");
         if(response.IsSuccessStatusCode){
             var content = await response.Content.ReadAsStringAsync();
-            var users = JsonSerializer.Deserialize<IEnumerable<UserViewModel>>(content);
+            var users = JsonConvert.DeserializeObject<IEnumerable<UserViewModel>>(content);
             return View("Index", users);
         }
         return View(new List<UserViewModel>());
     }
 
-    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult>Create(UserViewModel user)
+    {
+        if (ModelState.IsValid)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("/User", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }else{
+                ModelState.AddModelError(string.Empty,"Error creating User");
+            }
+        }
+        return View(user);
+    }
+
+    public async Task<IActionResult>Edit(int id)
+    {
+        var response = await _httpClient.GetAsync($"/User/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserViewModel>(content);
+            return View(user);
+        }else{
+            return RedirectToAction("Details");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult>Edit (int id, UserViewModel user)
+    {
+        if (ModelState.IsValid)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"User/{id}",content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index",new{id});
+            }else{
+                ModelState.AddModelError(string.Empty,"Error to update");
+            }
+        }
+        return View(user);
+    }
+
 }
